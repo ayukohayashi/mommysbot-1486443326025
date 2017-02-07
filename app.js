2d7ec3fddbb445b9770f92e695e2aee5ab068016
ayukohayashi <a-hayashi@nri.co.jp>
2017-02-07 14:06:58 +0900

    Update app.js

--- app.js
+++ app.js
@@ -15,6 +15,39 @@ var cfenv = require('cfenv');
 // create a new express server
 var app = express();
 
+//add2017/2/7
+var bodyParser = require('body-parser');
+
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+app.use(bodyParser.json());
+
+var request = require('request');
+
+app.post('/api', function(req, res) {
+  var options = {
+    method: 'POST',
+    uri: 'https://api.line.me/v2/bot/message/reply',
+    body: {
+      replyToken: req.body.events[0].replyToken,
+      messages: [{
+        type: "text",
+        text: req.body.events[0].message.text
+      }]
+    },
+    auth: {
+      bearer: 'bBy70HI0o27tUayJmRf5enI7Q7iBIPxaUg7UScUTRH5wHaPs0niDH90Npf6Q4Mju5HJ4wNIKahXaSqw4As1MCGKnsnrNu+6PI2wHs7VLyvgB1AakOirVxSdd4t9m4+z5G8UONQfbFVUQozq1tHLGugdB04t89/1O/w1cDnyilFU='
+    },
+    json: true
+  };
+  request(options, function(err, res, body) {
+    console.log(JSON.stringify(res));
+  });
+  res.send('OK');
+});
+//add end
+
 // serve the files out of ./public as our main files
 app.use(express.static(__dirname + '/public'));
 
